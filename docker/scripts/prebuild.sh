08dc4c7b9050d878ff7f2922601a0857eaf7e89d
Steven Freville <steven.f.13@gmail.com>
2023-10-26 13:37:41 +0200

    test

--- docker/scripts/prebuild.sh
+++ docker/scripts/prebuild.sh
@@ -17,40 +17,40 @@ echo '### BUILD CORE ###'
 cd $CORE_DIR && yarn install && yarn tsc --project tsconfig.build.json
 
 echo '### BUILD REQUIRED WORKSPACE FOR VITE APPS ###'
-cd $LIB_UTILS_DIR  && yarn workspace @leav/utils build
-cd $LIB_UI_DIR && yarn workspace @leav/ui build
+#cd $LIB_UTILS_DIR  && yarn workspace @leav/utils build
+#cd $LIB_UI_DIR && yarn workspace @leav/ui build
 
 echo '### BUILD VITE APPS ###'
 
 # Install & build
-cd $PORTAL_DIR && yarn install && yarn build
-rm -rf $CORE_PORTAL_DIST && mkdir -p $CORE_PORTAL_DIST
-cp -r $PORTAL_DIR/dist/* $CORE_PORTAL_DIST
+#cd $PORTAL_DIR && yarn install && yarn build
+#rm -rf $CORE_PORTAL_DIST && mkdir -p $CORE_PORTAL_DIST
+#cp -r $PORTAL_DIR/dist/* $CORE_PORTAL_DIST
 
 # Repeat for admin app
-cd $ADMIN_DIR && yarn install && yarn build
-rm -rf $CORE_ADMIN_DIST && mkdir -p $CORE_ADMIN_DIST
-cp -r $ADMIN_DIR/dist/* $CORE_ADMIN_DIST
+#cd $ADMIN_DIR && yarn install && yarn build
+#rm -rf $CORE_ADMIN_DIST && mkdir -p $CORE_ADMIN_DIST
+#cp -r $ADMIN_DIR/dist/* $CORE_ADMIN_DIST
 
 # Repeat for data-studio app
-cd $DATA_STUDIO_DIR && yarn install && yarn build
-rm -rf $CORE_DATA_STUDIO_DIST && mkdir -p $CORE_DATA_STUDIO_DIST
-cp -r $DATA_STUDIO_DIR/dist/* $CORE_DATA_STUDIO_DIST
+#cd $DATA_STUDIO_DIR && yarn install && yarn build
+#rm -rf $CORE_DATA_STUDIO_DIST && mkdir -p $CORE_DATA_STUDIO_DIST
+#cp -r $DATA_STUDIO_DIR/dist/* $CORE_DATA_STUDIO_DIST
 
 # Repeat for login app
-cd $LOGIN_DIR && yarn install && yarn build
-rm -rf $CORE_LOGIN_DIST && mkdir -p $CORE_LOGIN_DIST
-cp -r $LOGIN_DIR/dist/* $CORE_LOGIN_DIST
+#cd $LOGIN_DIR && yarn install && yarn build
+#rm -rf $CORE_LOGIN_DIST && mkdir -p $CORE_LOGIN_DIST
+#cp -r $LOGIN_DIR/dist/* $CORE_LOGIN_DIST
 
-echo '### GIT PROCESS ###'
+#echo '### GIT PROCESS ###'
 
 # Read version in package.json
-VERSION=$(cat $BASEDIR/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+#VERSION=$(cat $BASEDIR/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
 # Add /app/apps/core/applications and dist folders then commit
-cd $CORE_DIR && git add ./dist -f && git add ./applications -f && git commit -m "Update VITE apps dist folders for version: $VERSION"
+#cd $CORE_DIR && git add ./dist -f && git add ./applications -f && git commit -m "Update VITE apps dist folders for version: $VERSION"
 
-git push origin
+#git push origin
 
 #sleep 600
 
