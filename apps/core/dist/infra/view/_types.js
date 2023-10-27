ff6b3252da9abf52b54ce74cab1afccacd4d1fde
Steven Freville <steven.f.13@gmail.com>
2023-10-27 14:07:55 +0200

    test

--- apps/core/dist/infra/view/_types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VIEWS_COLLECTION_NAME = void 0;
-exports.VIEWS_COLLECTION_NAME = 'core_views';
--- apps/core/dist/interface/filesManager.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1({ 'core.app.core.filesManager': filesManager }) {
-    return {
-        init: () => filesManager.init()
-    };
-}
-exports.default = default_1;
