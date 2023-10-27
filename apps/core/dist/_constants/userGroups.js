78f540ff4f5bd81870bd7eaf53903b5a48526b53
Steven Freville <steven.f.13@gmail.com>
2023-10-27 11:51:58 +0200

    test

--- apps/core/dist/_constants/userGroups.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.filesAdminsGroupId = exports.adminsGroupId = void 0;
-// Copyright LEAV Solutions 2017
-// This file is released under LGPL V3
-// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-exports.adminsGroupId = '1';
-exports.filesAdminsGroupId = '2';
--- apps/core/dist/interface/tasksManager.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1({ 'core.app.core.tasksManager': tasksManager }) {
-    return {
-        initMaster: tasksManager.initMaster,
-        initWorker: tasksManager.initWorker
-    };
-}
-exports.default = default_1;
