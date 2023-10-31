b6d6974c20c72357491ac609099a917c1f7ecf90
Steven Freville <steven.f.13@gmail.com>
2023-10-31 11:24:46 +0100

    Add preload.js to download artifact and unzip it, it contains dist folder

--- docker/scripts/start-light-core.sh
+++ docker/scripts/start-light-core.sh
@@ -1,30 +1,17 @@
 # To avoid permissions issues later on, in particular during applications install
 #chown -R node:node /app
 
+yarn install
+
 git config --global --add safe.directory /app
 
-echo "Get the commit SHA"
-commit_sha="$(git rev-parse HEAD)"
-
-# Get artifact ID
-echo "Commit SHA: $commit_sha"
-artifact_url="https://api.github.com/repos/stevenfreville/leav-engine/actions/artifacts/dist-artifact-${commit_sha}/zip"
-
-echo "Artifact URL: $artifact_url"
-curl -s -H "Authorization: Bearer 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" -LJO "$artifact_url"
-
-echo "Unzip artifact"
-unzip -q dist-artifact-${commit_sha}.zip -d dist
-
-ls dist
-
-echo "Clean up"
-rm "dist-artifact-${commit_sha}".tar.gz
+# Download artifact and extract it (contains dist folders)
+node ./scripts/preload.js
 
 # Install dependencies
-yarn workspaces focus core --production
+#yarn workspaces focus core --production
 
-#mkdir -p /app/apps/core/dist/plugins
+mkdir -p /app/apps/core/dist/plugins
 
 yarn run start --server
-sleep 60000
+#sleep 60000
